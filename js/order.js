d5a22c1c3d8802c6dd947bd5c4c57a9a0f698259
Willie Taing <taingw4@uw.edu>
2013-11-23 10:35:42 -0800

    Add pizza to cart console log info correct

--- js/order.js
+++ js/order.js
@@ -26,13 +26,14 @@ function renderPizzas() {
 	    instance.find('.pizza-name').html(pizza.name);
 	    instance.find('.pizza-description').html(pizza.description);
 	    //instance.find('.pizza-prices').html('$ ' + pizza.prices[0] + ' / ' + pizza.prices[1] + ' / ' + pizza.prices[2]);
+	    instance.find('form').attr('name',pizza.name);
 	    instance.find('#small').html('Small $' + pizza.prices[0]);
 	    instance.find('#medium').html('Medium $' + pizza.prices[1]);
 	    instance.find('#large').html('Large $' + pizza.prices[2]);
-	    instance.find('#small').attr('value',pizza.prices[0]);
-	    instance.find('#medium').attr('value',pizza.prices[1]);
-	    instance.find('#large').attr('value',pizza.prices[2]);
-	    instance.find('.quantity').attr('name',pizza.name);
+	    instance.find('#small').val(pizza.prices[0]);
+	    instance.find('#medium').val(pizza.prices[1]);
+	    instance.find('#large').val(pizza.prices[2]);
+	    instance.find('.add-pizza').attr('name',pizza.name);
 	    instance.removeClass('pizza-template');
         $('.pizzas').append(instance);
 	} 	
@@ -51,6 +52,7 @@ function renderDrinks() {
     	instance.find('.drink-price').html('$' + drink.price);
     	instance.find('.quantity').attr('name',drink.name);
     	instance.find('.quantity').attr('price',drink.price);
+    	instance.find('.add-other').attr('name',drink.name);
     	instance.removeClass('drink-template');
     	$('.drinks').append(instance);
 	} 
@@ -68,19 +70,33 @@ function renderDesserts() {
     	instance.find('.dessert-price').html('$' + dessert.price);
     	instance.find('.quantity').attr('name',dessert.name);
     	instance.find('.quantity').attr('price',dessert.price);
+    	instance.find('.add-other').attr('name',dessert.name);
     	instance.removeClass('dessert-template');
     	$('.desserts').append(instance);
 	} 
 }
 
 function addPizza() {
-	alert(this.find('quantity').getAttribute('class'));
+	var cart = {
+    	name: null,
+        address1: null,
+        zip: null,
+        phone: null,
+        items: [] //empty array
+    }; //cart data
+
+	var name = this.getAttribute('name');
+	console.log(this.getAttribute('item-type'));
+	console.log(name);
+	console.log($("form[name='" + name + "'] .size :selected").attr('id'));
+	console.log($("form[name='" + name + "'] .size").val());
+	console.log($("form[name='" + name + "'] .quantity").val());
 	var newCartItem = {
-		type: $('.quantity').getAttribute('item-type'),
-		name: $('.quantity').getAttribute('name'),
-		size: $('.size').getAttribute('id'),
-		price: $('.size').getAttribute('value'),
-		quantity: $('.quantity').getAttribute('value')
+		type: this.getAttribute('item-type'),
+		name: name,
+		size: $("form[name='" + name + "'] .size :selected").attr('id'),
+		price: $("form[name='" + name + "'] .size").val(),
+		quantity: $("form[name='" + name + "'] .quantity").val()
 	}
 	cart.items.push(newCartItem);
 	renderCart(cart, $('.cart'));
@@ -102,7 +118,7 @@ function renderCart(cart, container) {
 	var item;
 	var instance;
 	var template = $('.cart-template');
-	container.empty();
+	//container.empty();
 	for (idx = 0; idx < cart.items.length; idx++) {
 		item = cart.items[idx];
 		instance = template.clone();
